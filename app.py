b9b6c313b1da06ea7986707c355a8541db3633fd
Afash <afashseven@gmail.com>
2024-08-18 17:11:36 +0330

    base structure with templates and statics

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, unique=True, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.now)
+    
+    def __repr__(self):
+        return f"Todo({self.id} - {self.content} - {self.date})"
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', name = 'amir')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
