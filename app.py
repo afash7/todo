8dfb532e03051c89f1dcf89824cd269fde1293ae
Afash <afashseven@gmail.com>
2024-08-19 18:25:37 +0330

    db instance created

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 
 
@@ -28,4 +28,6 @@ def about():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
